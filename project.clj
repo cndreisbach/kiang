efc5eed3e07d49bfe7fec40eddba74f0c39fbd53
Clinton R. Nixon <crnixon@gmail.com>
2012-01-10 22:21:03 -0500

    Added docs

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject kiang "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Kiang is a naive Bayesian filtering system for determining the programming language of a code sample."
   :dependencies [[org.clojure/clojure "1.3.0"]])
